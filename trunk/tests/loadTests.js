d9923d5f310a38a2c15cb56dcad7b92c4f398e36
nick.rabinowitz <nick.rabinowitz@db8a699f-bd44-0410-b7cb-115e62ebdaf9>
2008-05-31 18:01:04 +0000

    adding a basic set of jsUnit unit tests. Unit testing is my new favorite thing in the world.

---
+++ trunk/tests/loadTests.js
@@ -0,0 +1,38 @@
+function testDatasetIsDefined() {
+    assertNotUndefined("dataset is defined", tm.datasets["ds0"]);
+}
+
+function testItemLoaded() {
+    var ds = tm.datasets["ds0"];
+    assertEquals("one item in item array", ds.getItems().length, 1);
+}
+
+function testItemLoadedInEventSource() {
+    var ds = tm.datasets["ds0"];
+    assertEquals("one item in eventSource", ds.eventSource.getCount(), 1);
+}
+
+function testEarliestDate() {
+    var ds = tm.datasets["ds0"];
+    assertEquals("year matches", ds.eventSource.getEarliestDate().getFullYear(), 1980);
+    assertEquals("month matches", ds.eventSource.getEarliestDate().getMonth(), 0);
+    // Timeline seems to adjust for the timezone after parsing :(
+    assertEquals("day matches", ds.eventSource.getEarliestDate().getDate(), 1);
+}
+
+function testLatestDate() {
+    var ds = tm.datasets["ds0"];
+    assertEquals("year matches", ds.eventSource.getLatestDate().getFullYear(), 2000);
+    assertEquals("month matches", ds.eventSource.getEarliestDate().getMonth(), 0);
+    // Timeline seems to adjust for the timezone after parsing :(
+    assertEquals("day matches", ds.eventSource.getEarliestDate().getDate(), 1);
+}
+
+function testItemAttributes() {
+    var items = tm.datasets["ds0"].getItems();
+    var item = items[0];
+    assertEquals("title matches", item.getTitle(), "Test Event");
+    assertEquals("placemark type matches", item.getType(), "marker");
+    var point = new GLatLng(23.456, 12.345);
+    assertTrue("point matches", item.getInfoPoint().equals(point));
+}
