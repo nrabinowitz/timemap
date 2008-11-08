97f0d0a9dde0744bb6ab55152ab30073b1059933
nick.rabinowitz <nick.rabinowitz@db8a699f-bd44-0410-b7cb-115e62ebdaf9>
2008-11-08 18:19:53 +0000

    Relicensing with the MIT license - see LICENSE.txt

--- trunk/timemap.js
+++ trunk/timemap.js
@@ -10,19 +10,6 @@
  * The TimeMap object is intended to sync a SIMILE Timeline with a Google Map.
  * Dependencies: Google Maps API v2, SIMILE Timeline v1.2
  * Thanks to Jörn Clausen (http://www.oe-files.de) for initial concept and code.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *---------------------------------------------------------------------------*/
 
  
