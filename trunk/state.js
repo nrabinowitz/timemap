cd5078b456cfb9bdbe246352830c9f48411b047b
nick.rabinowitz <nick.rabinowitz@db8a699f-bd44-0410-b7cb-115e62ebdaf9>
2010-02-01 19:55:51 +0000

    several small tweaks, fleshing out state functions

--- trunk/state.js
+++ trunk/state.js
@@ -10,27 +10,7 @@
  *
  * @author Nick Rabinowitz (www.nickrabinowitz.com)
  
- 
-Here's the basic idea - you've got an array of state setters, consisting of a key and a function:
 
-TimeMap.state.setters = [ 
-    'mapZoom': function(tm, value) {
-        // set the map zoom according to value
-        // tm == TimeMap object
-    },
-    // etc
-]
-
-users can manipulate this or add their own functions.
-
-then a TimeMap.setState() function:
-
-TimeMap.prototype.setState = function(state) {
-    // state is a config object
-    // go through each key in state, if there's a corresponding state.setters key,
-    // run that function with the config value:
-    TimeMap.state.setters[key](this, state[key]);
-}
 
 then a function to get the state config from the URL (probably the hash, right?)
 
@@ -39,19 +19,191 @@ TimeMap.state.getFromUrl = function() {
     // return a config object
 }
 
-so to set state from the URL you have another function:
-
-TimeMap.setStateFromUrl = function() {
-    this.setState(TimeMap.state.getFromUrl());
-}
-
-then you need a corresponding set of state serializers, that can turn the 
-current state into a config object
-
-TimeMap.state.serializers = [
-    function(tm, config) {
-        // get state from tm, add to config as value
-    }
-]
- 
  */
+
+/*----------------------------------------------------------------------------
+ * Core TimeMap state methods
+ *---------------------------------------------------------------------------*/
+
+/**
+ * Set the timemap state with a set of configuration options.
+ *
+ * @param {Object} state    Object with state config settings
+ */
+TimeMap.prototype.setState = function(state) {
+    var setters = TimeMap.state.setters,
+        key;
+    // go through each key in state
+    for (key in state) {
+        if (state.hasOwnProperty(key)) {
+            if (key in setters) {
+                // run setter function with config value
+                setters[key](this, state[key]);
+            }
+        }
+    }
+};
+
+/**
+ * Get a configuration object of state variables
+ *
+ * @return {Object}     Object with state config settings
+ */
+TimeMap.prototype.getState = function() {
+    var state = {},
+        serializers = TimeMap.state.serializers,
+        key;
+    // run through serializers, adding values to state
+    for (key in serializers) {
+        if (serializers.hasOwnProperty(key)) {
+            // run serializer to get state value
+            state[key] = serializers[key](this);
+        }
+    }
+    return state;
+};
+
+/**
+ * Set the timemap state with parameters in the URL
+ */
+TimeMap.prototype.setStateFromUrl = function() {
+    this.setState(TimeMap.state.getFromUrl());
+};
+
+/**
+ * Get URL with current state parameters in hash
+ *
+ * @return {String}     URL with state parameters
+ */
+TimeMap.prototype.getStateUrl = function() {
+    // XXX
+};
+
+/*----------------------------------------------------------------------------
+ * State namespace
+ *---------------------------------------------------------------------------*/
+
+/**
+ * @namespace
+ * Namespace for static state functions
+ */
+TimeMap.state = {
+
+    /**
+     * @namespace
+     * Map of setter functions, each taking a TimeMap object and a value.
+     * Add your own functions to this object to set additional state variables.
+     */
+    setters: {
+        /**
+         * @function
+         * Set the map zoom level
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @param {String} value    Zoom level
+         */
+        mapZoom: function(tm, value) {
+            tm.map.setZoom(parseInt(value));
+        },
+        
+        /**
+         * @function
+         * Set the map center
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @param {String} value    Map center ("lat,lon" or "lat,lon,zoom")
+         */
+        mapCenter: function(tm, value) {
+            var params = value.split(","),
+                point, zoom;
+            if (params.length < 2) {
+                // give up
+                return;
+            }
+            point = new GLatLng(
+                parseFloat(params[0]),
+                parseFloat(params[1])
+            );
+            if (params[3]) {
+                zoom = parseInt(params[3]);
+            }
+            tm.map.setCenter(point, zoom);
+        },
+        
+        /**
+         * @function
+         * Set the timeline center date
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @param {String} value    Date to set (one of the strings accepted by TimeMap#parseDate)
+         */
+        date: function(tm, value) {
+            tm.scrollToDate(value);
+        },
+        
+        /**
+         * @function
+         * Select/open a particular item
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @param {String} value    Index of item to select
+         */
+        selected: function(tm, value) {
+            var index = parseInt(value),
+                item = tm.getItems()[index];
+            if (item) {
+                item.openInfoWindow();
+            }
+        }
+    },
+
+    /**
+     * @namespace
+     * Map of serializer functions, each returning the value of the current state
+     * as a string.  Add your own functions to this object to serialize additional 
+     * state variables.
+     */
+    // XXX
+    serializers: {
+        /**
+         * @function
+         * Get the map zoom level
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @return {String}         Zoom level
+         */
+        mapZoom: function(tm, value) {
+        },
+        
+        /**
+         * @function
+         * Get the map center
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @return {String}         Map center ("lat,lon" or "lat,lon,zoom")
+         */
+        mapCenter: function(tm, value) {
+        },
+        
+        /**
+         * @function
+         * Get the timeline center date
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @return {String}         Date to set (one of the strings accepted by TimeMap#parseDate)
+         */
+        date: function(tm, value) {
+        },
+        
+        /**
+         * @function
+         * Get the selected item, if any
+         * 
+         * @param {TimeMap} tm      TimeMap object
+         * @return {String}         Index of item to select
+         */
+        selected: function(tm) {
+        }
+    }    
+};
+
