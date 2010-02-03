6ccbcf6f6ac5868244dcafb9b1e65ad1622274dc
nick.rabinowitz <nick.rabinowitz@db8a699f-bd44-0410-b7cb-115e62ebdaf9>
2010-02-03 21:06:59 +0000

    fleshing out state functions

--- trunk/state.js
+++ trunk/state.js
@@ -14,10 +14,7 @@
 
 then a function to get the state config from the URL (probably the hash, right?)
 
-TimeMap.state.getFromUrl = function() {
-    // parse location.hash and extract key/value pairs;
-    // return a config object
-}
+
 
  */
 
@@ -80,7 +77,7 @@ TimeMap.prototype.getStateUrl = function() {
 };
 
 /*----------------------------------------------------------------------------
- * State namespace
+ * State namespace, with setters, serializers, and url functions
  *---------------------------------------------------------------------------*/
 
 /**
@@ -96,24 +93,22 @@ TimeMap.state = {
      */
     setters: {
         /**
-         * @function
          * Set the map zoom level
          * 
          * @param {TimeMap} tm      TimeMap object
          * @param {String} value    Zoom level
          */
-        mapZoom: function(tm, value) {
+        zoom: function(tm, value) {
             tm.map.setZoom(parseInt(value));
         },
         
         /**
-         * @function
          * Set the map center
          * 
          * @param {TimeMap} tm      TimeMap object
          * @param {String} value    Map center ("lat,lon" or "lat,lon,zoom")
          */
-        mapCenter: function(tm, value) {
+        center: function(tm, value) {
             var params = value.split(","),
                 point, zoom;
             if (params.length < 2) {
@@ -131,7 +126,6 @@ TimeMap.state = {
         },
         
         /**
-         * @function
          * Set the timeline center date
          * 
          * @param {TimeMap} tm      TimeMap object
@@ -142,7 +136,6 @@ TimeMap.state = {
         },
         
         /**
-         * @function
          * Select/open a particular item
          * 
          * @param {TimeMap} tm      TimeMap object
@@ -163,47 +156,99 @@ TimeMap.state = {
      * as a string.  Add your own functions to this object to serialize additional 
      * state variables.
      */
-    // XXX
     serializers: {
         /**
-         * @function
          * Get the map zoom level
          * 
          * @param {TimeMap} tm      TimeMap object
          * @return {String}         Zoom level
          */
-        mapZoom: function(tm, value) {
+        zoom: function(tm) {
+            return tm.map.getZoom();
         },
         
         /**
-         * @function
          * Get the map center
          * 
          * @param {TimeMap} tm      TimeMap object
          * @return {String}         Map center ("lat,lon" or "lat,lon,zoom")
          */
-        mapCenter: function(tm, value) {
+        center: function(tm) {
+            var center = tm.map.getCenter();
+            return center.lat() + "," + center.lng();
         },
         
         /**
-         * @function
          * Get the timeline center date
          * 
          * @param {TimeMap} tm      TimeMap object
-         * @return {String}         Date to set (one of the strings accepted by TimeMap#parseDate)
+         * @return {String}         Timeline center date, in ISO8601 format
          */
-        date: function(tm, value) {
+        date: function(tm) {
+            return TimeMap.util.formatDate(tm.timeline.getBand(0).getCenterVisibleDate());
         },
         
         /**
-         * @function
          * Get the selected item, if any
          * 
          * @param {TimeMap} tm      TimeMap object
          * @return {String}         Index of item to select
          */
         selected: function(tm) {
+            var x = 0;
+            tm.eachItem(function(item) {
+                if (item.selected) {
+                    return x;
+                }
+                x++;
+            });
         }
-    }    
+    },
+    
+    /**
+     * Get the state parameters from the URL, returning as a config object
+     * 
+     * @return {Object}             Object with state config settings
+     */
+    fromUrl: function() {
+        var paramPairs = location.hash.substring(1).split('&'),
+            state = {}, x, param;
+        for (x=0; x < paramPairs.length; x++) {
+            if (paramPairs[x] != "") {
+                param = paramPairs[x].split('=');
+                state[param[0]] = decodeURIComponent(param[1]);
+            }
+        }
+        return state;
+    },
+    
+    /**
+     * Make a parameter string from a state object
+     *
+     * @param {Object} state        Object with state config settings
+     * @return {String}             Parameter string in URL param format
+     */
+    toParamString: function(state) {
+        var params = [], key;
+        // go through each key in state
+        for (key in state) {
+            if (state.hasOwnProperty(key)) {
+                params.append(key + "=" + encodeURIComponent(state[key]);
+            }
+        }
+        return params.join("&");
+    },
+    
+    /**
+     * Make a full URL from a state object
+     *
+     * @param {Object} state        Object with state config settings
+     * @return {String}             Full URL with parameters
+     */
+    toUrl: function(state) {
+        var paramString = TimeMap.state.toParamString(state),
+            url = location.href.split("#")[0];
+        return url + "#" + paramString;
+    }
+    
 };
-
